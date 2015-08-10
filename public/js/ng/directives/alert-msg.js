8b04e0091232b1838c7d839d83391c7b97737595
l-azai <s.wai@outlook.com>
2015-08-10 22:03:01 +0100

    back link and start on alert directive

---
+++ public/js/ng/directives/alert-msg.js
@@ -0,0 +1,10 @@
+(function(){
+    var alertDirective = function() {
+        return {
+            
+        };
+    };
+
+    angular.module("medisApp.directives")
+        .directive("alert-msg", alertDirective);
+}());

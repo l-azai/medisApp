6dd2d7ae2640e2b65f17bc9313365cb6c62e1300
l-azai <s.wai@outlook.com>
2015-08-16 18:04:16 +0100

    tweaked alert-msg directive

--- public/js/ng/directives/alert-msg.js
+++ public/js/ng/directives/alert-msg.js
@@ -8,33 +8,35 @@
                 closeTempMsg: '&'
             },
             controller: ["$scope", function($scope) {
-                // TODO: possibly change how type is passed. Instead of explicitly setting on html,
-                // can make controller pass an object with the type and msg. $watch for the object and
-                // dynamically set type and set message.
-                var alertHeading;
-                var glyph;
+                $scope.$watch('type', function(value) {
+                    setTempMsg(value);
+                });
 
-                switch(angular.lowercase($scope.type)) {
-                    case 'success':
-                        alertHeading = 'Success';
-                        glyph = 'ok-sign';
-                    break;
-                    case 'danger':
-                        alertHeading = 'Error';
-                        glyph = 'remove-sign';
-                    break;
-                    case 'warning':
-                        alertHeading = 'Warning';
-                        glyph = 'exclamation-sign';
-                    break;
-                    case 'info':
-                        alertHeading = 'Info';
-                        glyph = 'info-sign';
-                    break;
-                }
+                var setTempMsg = function(type) {
+                    var alertHeading, glyph;
 
-                $scope.alertHeading = alertHeading;
-                $scope.glyph = glyph;
+                    switch(type) {
+                        case 'success':
+                            alertHeading = 'Success';
+                            glyph = 'ok-sign';
+                        break;
+                        case 'danger':
+                            alertHeading = 'Error';
+                            glyph = 'remove-sign';
+                        break;
+                        case 'warning':
+                            alertHeading = 'Warning';
+                            glyph = 'exclamation-sign';
+                        break;
+                        case 'info':
+                            alertHeading = 'Info';
+                            glyph = 'info-sign';
+                        break;
+                    }
+
+                    $scope.alertHeading = alertHeading;
+                    $scope.glyph = glyph;
+                };
             }],
             templateUrl: '/partials/shared/alert-msg.html'
         };
